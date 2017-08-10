6a9c93fa134f2ba2533106702e1cba5de32315d3
Tushar Pokle <tushar.pokle@gmail.com>
2017-08-10 23:03:25 +1000

    version

--- args.js
+++ args.js
@@ -3,16 +3,17 @@ const commandLineArgs = require('command-line-args')
 const optionDefinitions = [
   { name: 'headers', alias: 'h', type: Boolean },
   { name: 'format', alias: 'f', type: String },
-  { name: 'help', type: Boolean }
+  { name: 'help', type: Boolean },
+  { name: 'version', alias: 'v', type: Boolean }
 ]
 
 const options = commandLineArgs(optionDefinitions, { partial: true });
 
-if (options.help) {
-  console.log('csvfmt usage: https://github.com/pokle/csvfmt')
+if (options.help || options.version) {
+  console.error('csvfmt', require(require('path').join(__dirname, 'package.json')).version)
+  console.error('Usage at: https://github.com/pokle/csvfmt')
   process.exit(0);
 }
-
 
 
 options.files = options._unknown || []
